d12a29423ed1e0389990472a2f3005af067df690
fumihiko kakuma <kakuma@valinux.co.jp>
2014-10-15 08:49:56 +0900

    rename puppet/modules/jenkins to puppet/modules/jenkins_3p. add jenkins-cli.jar and do reboot

--- puppet/modules/os_ext_testing/manifests/devstack_slave.pp
+++ puppet/modules/os_ext_testing/manifests/devstack_slave.pp
@@ -12,16 +12,82 @@ class os_ext_testing::devstack_slave (
   $devstack_gate_3pprj_base = '',
   $devstack_gate_3pbranch = '',
 ) {
-  class { 'os_ext_testing::base':
-    project_config_repo => $project_config_repo,
-  }
   include openstack_project::tmpcleanup
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [],
+    certname                  => $certname,
+    sysadmins                 => $sysadmins,
+  }
+
   class { 'jenkins::slave':
-    bare         => $bare,
     ssh_key      => $ssh_key,
     python3      => $python3,
-    include_pypy => $include_pypy,
-    jenkins_url  => $jenkins_url,
   }
-  include devstack_host
+
+  class { 'openstack_project::slave_common':
+    include_pypy        => $include_pypy,
+    project_config_repo => $project_config_repo,
+  }
+
+  if (! $thin) {
+    include openstack_project::thick_slave
+  }
+
+  # Although we don't use Nodepool itself, we DO make use of some
+  # of the scripts that are housed in the nodepool openstack-infra/config
+  # files directory.
+  file { '/opt/nodepool-scripts':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    recurse => true,
+    purge   => true,
+    force   => true,
+    source  => $::project_config::nodepool_scripts_dir,
+  }
+
+  file { '/usr/local/jenkins/jenkins-cli.jar':
+    ensure  => present,
+    source  => 'puppet:///modules/jenkins_3p/jenkins-slave/jenkins-cli.jar',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/usr/local/jenkins'],
+  }
+
+  file { '/usr/local/bin/rebuild-node.sh':
+    ensure => present,
+    source => 'puppet:///modules/jenkins_3p/rebuild-node.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  if ($::osfamily == 'Debian') {
+
+    file { '/etc/init.d/jenkins-slave':
+      ensure => present,
+      source => 'puppet:///modules/jenkins_3p/jenkins-slave/init',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      require => File ['/usr/local/jenkins/jenkins-cli.jar']
+    }
+    file { '/etc/default/jenkins-slave':
+      ensure => present,
+      content => template('jenkins_3p/jenkins-slave/default.erb'),
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      require => File ['/etc/init.d/jenkins-slave']
+    }
+    exec { 'update-rc':
+      command => '/usr/sbin/update-rc.d jenkins-slave defaults 99 01',
+      path    => ['/sbin', '/bin', '/usr/sbin', '/usr/bin'],
+      require => File ['/etc/init.d/jenkins-slave']
+    }
+  }
+
 }
